93171d1064000632e2838ca2e6c65e68a2263f64
Stricted <info@stricted.net>
2018-10-01 11:13:05 +0200

    universal8890: build power hal
    Change-Id: Ie8a4b190f53858feb89c942f6e6c9cff4a348985

--- device-common.mk
+++ device-common.mk
@@ -87,6 +87,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl
 
+# Power
+PRODUCT_PACKAGES += \
+    power.exynos5 \
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
