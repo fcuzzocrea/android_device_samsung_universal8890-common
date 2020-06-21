cd74c88d6f201a247f3294d05eee05ca5dc8d457
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-06-21 23:21:08 +0200

    widevine 1.2

--- device-common.mk
+++ device-common.mk
@@ -96,9 +96,14 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0 \
+    android.hardware.drm@1.1 \
+    libfwdlockengine \
+    libdrmclearkeyplugin \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.2-service.widevine
     
 # DTB tool    
 PRODUCT_HOST_PACKAGES += \
