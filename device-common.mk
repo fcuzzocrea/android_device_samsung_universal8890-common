c738aa6d8952bb6b1fe51408dabbc5a187a733b8
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-06-22 09:02:32 +0200

    unpin widevine

--- device-common.mk
+++ device-common.mk
@@ -102,8 +102,7 @@ PRODUCT_PACKAGES += \
     libdrmclearkeyplugin \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.2-service.widevine
+    android.hardware.drm@1.0-impl
     
 # DTB tool    
 PRODUCT_HOST_PACKAGES += \
